12e35b6b50de879918cc549f5578fc2623c5a353
DIBYAJYOTI MISHRA <dibyajyotimishra14.com>
2021-05-09 01:03:49 +0530

    ⚡️ chore: update rest api to graphql

--- App.js
+++ App.js
@@ -1,33 +1,151 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+/* eslint-disable no-alert */
 
-//Screens
-import {Signup} from './screens/';
-import {Scan} from './screens';
+import React, {Component} from 'react';
+import {StyleSheet, Button, Text, View} from 'react-native';
+import {RNCamera} from 'react-native-camera';
+import Axios from 'axios';
 
-const Stack = createStackNavigator();
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.camera = null;
+    this.barcodeCodes = [];
+    this.data = '';
 
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Signup"
-          component={Signup}
-          options={{headerShown: false}}
+    this.state = {
+      sugars: '',
+    };
+  }
+
+  onBarCodeRead(scanResult) {
+    console.info(scanResult.bounds.origin);
+    this.data = scanResult.data;
+    this.fetchData();
+    if (scanResult.data != null) {
+      if (!this.barcodeCodes.includes(scanResult.data)) {
+        this.barcodeCodes.push(scanResult.data);
+      }
+    }
+    return;
+  }
+
+  async fetchData() {
+    let barcode = this.data;
+    // let barcode = '8908006077410';
+    // const res = await Axios.post(
+    //   'https://palm-surf-hellebore.glitch.me/food/barcode',
+    //   {barcode: Barcode},
+    // );
+    const body = {
+      query: `
+        query{
+          food(barcode:"${barcode}"){
+            Sugars
+          }
+        }  
+      `,
+    };
+
+    const res = await Axios.post(
+      'https://adaptable-silent-nautilus.glitch.me/api',
+      body,
+      (headers = {
+        'Content-Type': 'application/json',
+      }),
+    );
+    // console.log('-----------------------');
+    // console.log(res.data.Sugars);
+    // console.error(res.data.data.food.Sugars);
+    // console.log('------------------------');
+    this.setState({
+      sugars: JSON.stringify(res.data.data.food.Sugars),
+    });
+    return Barcode;
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          type={RNCamera.Constants.Type.back}
+          defaultTouchToFocus
+          flashMode={RNCamera.Constants.FlashMode.auto}
+          mirrorImage={false}
+          onBarCodeRead={this.onBarCodeRead.bind(this)}
+          androidCameraPermissionOptions={{
+            title: 'Camera Access Request',
+            message: 'Hi, Can I access Your Camera ?',
+            buttonPositive: 'Yes',
+            buttonNegative: 'No',
+          }}
+          style={styles.preview}
         />
-        <Stack.Screen
-          name="Screen"
-          component={Screen}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+        <View style={[styles.overlay, styles.topOverlay]}>
+          <Text style={styles.resultMessage}>
+            Hey mate, scan your barcode to get the data.
+          </Text>
+        </View>
+        <View style={[styles.overlay, styles.bottomOverlay]}>
+          <Button
+            onPress={() => {
+              this.data !== ''
+                ? alert('Sugar content \t' + this.state.sugars)
+                : alert('Scan and then try');
+            }}
+            style={styles.viewResultsButton}
+            title={'View Results'}
+          />
+        </View>
+      </View>
+    );
+  }
+}
 
-export default () => {
-  return <App />;
-};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    padding: 16,
+    right: 0,
+    left: 0,
+    alignItems: 'center',
+  },
+  topOverlay: {
+    top: 0,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  bottomOverlay: {
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  viewResultsButton: {
+    padding: 15,
+    backgroundColor: 'white',
+    borderRadius: 40,
+  },
+  resultMessage: {
+    fontSize: 14,
+    color: 'white',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
--- screens/index.js
+++
@@ -1,4 +0,0 @@
-import Signup from './SignupScreen';
-import Scan from './ScanScreen';
-
-export {Signup, Scan};
